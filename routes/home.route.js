05f253ab611bdbe903af64fd288108e41d090686
Dat Nguyen <dathhcc2@gmail.com>
2023-10-11 15:26:15 +0700

    fix home router

--- routes/home.route.js
+++ routes/home.route.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.post('/get', (req, res) => {
+router.get('/', (req, res) => {
     return res.json({ msg: 'Connect success' });
 });
 
